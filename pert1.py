264826c7608db0518108eb9ce40ddfc4083700e2
algimaulana_alprog <malgimaulana6@gmail.com>
2025-11-09 19:38:55 +0800

    Add files via upload

---
+++ pert1.py
@@ -0,0 +1,12 @@
+#python dijalankan dari kiri ke kanan
+#pythhon dijalankan dari atas kebawah
+
+print ("hello world")
+print ("saya muhammad algi")
+print ("prodi PTI")
+print ("kelas D")
+print ("sangat ganteng")
+
+x=6
+y=8
+print (x + y)
---
+++ pert2.py
@@ -0,0 +1,9 @@
+#variabel
+
+x = 81
+y = 56
+besar = 66
+
+print ("nilai dari x =" , x)
+print ("nilai dari x + y =" , x + y)
+print ("nilai dari x + y + besar=" , x + y + besar)
