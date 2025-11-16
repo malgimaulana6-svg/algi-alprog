26f1bfe9b72da642fccdf8ed8e5e3cf2e6f6ba13
algimaulana_alprog <malgimaulana6@gmail.com>
2025-11-16 23:37:34 +0800

    Add files via upload
    tugas pert4

---
+++ tuagspert4.py
@@ -0,0 +1,12 @@
+nama = input("masukkan nama anda :")
+print ("nama anda adalah" , nama)
+print(type(nama))
+tipeData = input("masukkan tipe data nama anda :")
+print("tipe data nama anda adalah" , tipeData)
+
+nim = int(input("masukkan nim anda :"))
+print ("nim anda adalah" , nim)
+print(type(nim))
+tipeData = input("masukkan tipe data nim anda :")
+print("tipe data nim anda adalah" , tipeData)
+
